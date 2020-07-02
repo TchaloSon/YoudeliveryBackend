4dfafb2dfedf29f25819f523134e6225c34bed32
Aboudourazakou <tetereou2017@gmail.com>
2020-07-02 12:01:40 +0200

    modifier

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,65 @@
+<?php
+namespace App\Http\Controllers\API;
+use Illuminate\Http\Request; 
+use App\Http\Controllers\Controller; 
+use Illuminate\Support\Facades\Auth; 
+use App\User; 
+use Validator;
+class AuthController extends Controller 
+{
+  /** 
+   * Login API 
+   * 
+   * @return \Illuminate\Http\Response 
+   */ 
+  public function login(Request $request){ 
+    if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){ 
+      $user = Auth::user(); 
+      $success['token'] =  $user->createToken('LaraPassport')->accessToken; 
+      return response()->json([
+        'status' => 'success',
+        'data' => $success
+      ]); 
+    } else { 
+      return response()->json([
+        'status' => 'error',
+        'data' => 'Unauthorized Access'
+      ]); 
+    } 
+  }
+
+  /** 
+   * Register API 
+   * 
+   * @return \Illuminate\Http\Response 
+   */ 
+  public function register(Request $request) 
+  { 
+    $validator = Validator::make($request->all(), [ 
+      'nom' => 'required', 
+      'telephone'=>'required',
+      'prenom'=>'required',
+      'adresse'=>'required',
+      'email' => 'required|email', 
+      'password' => 'required', 
+      'c_password' => 'required|same:password', 
+    ]);
+    if ($validator->fails()) { 
+      return response()->json(['error'=>$validator->errors()]);
+    }
+    $postArray = $request->all(); 
+    $postArray['password'] = bcrypt($postArray['password']); 
+    $user = User::create($postArray); 
+    $success['token'] =  $user->createToken('LaraPassport')->accessToken; 
+    $success['nom'] =  $user->nom;
+    $success['prenom'] =  $user->prenom;
+    $success['adresse'] =  $user->adresse;
+    $success['telephone'] =  $user->telephone;
+    $success['email'] =  $user->email;
+  
+    return response()->json([
+      'status' => 'success',
+      'data' => $success,
+    ]); 
+  }
+}
--- routes/api.php
+++ routes/api.php
@@ -24,4 +24,15 @@ Route::get('/Utilisateur',['uses'=>'InscriptionController@utilisateurGet']);
 Route::put('/Utilisateur',['uses'=>'InscriptionController@utilisateurModifier']);
 Route::delete('/Utilisateur',['uses'=>'InscriptionController@utilisateurEffacer']);
 
-Route::post('/register',['uses'=>'RegisterController@create']);
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+Route::post('login', 'API\AuthController@login');
+Route::post('register', 'API\AuthController@register');
